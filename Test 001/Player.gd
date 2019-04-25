501061da08750df66d5543d3c6a7b5e651f76b56
Rincewind70 <49660297+Rincewind70@users.noreply.github.com>
2019-04-25 18:19:22 +0200

    Fixed class and function to work properly
    Anyway, I guess I will revert this to not using a special class. There is no real benefit.

--- Test 001/Player.gd
+++ Test 001/Player.gd
@@ -1,7 +1,10 @@
 extends KinematicBody2D
 
-export var player_name : String = "Astronaut"
+# Contants
+const UP = Vector2(0,-1)
 
+# Public properties
+export var player_name : String = "Astronaut"
 export var MAX_SPEED : float = 100
 export var ACCELERATION : float = 500
 export var GRAVITY : float = 600
@@ -10,55 +13,46 @@ export var MAX_JUMP : float = 350
 export var DELTA_JUMP : float = 30
 
 
+# Variable to hold current user input
 var input_direction : int = 0
-var direction : int = 0
-var speed : float = 0
-var jump : float = 0
-var can_jump = true
 
-class Movement : 
+# Class to orginize all variables
+# using class just to organize and enabling all vars 
+# to be sent to funcitons by reference
+# (Just a test)
+class Movement: 
 	var motion = Vector2()
 	var direction : int = 0
 	var can_jump : bool = true
 	var jump : float = 0
 	var speed : float = 0
-	var animationsprite = Node2D
+	var animationsprite : AnimatedSprite
 	
-	func _init():
-		return self
-	func Movement(c : Node2D):
-		animationsprite = c
-		return self
-
-var movement : Movement
-
-const UP = Vector2(0,-1)
-var motion : Vector2
-
-
+# The instance variable for the class
+var movement : Movement = Movement.new()
 
 func _ready():
-	movement = Movement(get_node("AnimatedSprite"))
+	# Settin up
 	movement.animationsprite = get_node("AnimatedSprite")
-
+	
 func _process(delta):	
-
+	
 	movement.can_jump = is_on_floor()
 	
 	# Input
-	movement.direction = get_input()
-
+	get_input(movement)
+	
 	# Movement 
-	calc_movement(delta)
+	calc_movement(delta,movement)
 	
 	# Animation
-	play_animation(movement.direction,movement.speed,is_on_floor())	
+	play_animation(movement,is_on_floor())	
 
 
-func get_input():
+func get_input(m:Movement):
 	var last_direction : int = 0
 	if input_direction:
-		last_direction = input_direction
+		m.direction = input_direction
 	
 	input_direction = 0
 	if Input.is_action_pressed("ui_left"):
@@ -67,28 +61,27 @@ func get_input():
 	if Input.is_action_pressed("ui_right") :
 		input_direction = 1
 	
-	if movement.can_jump:
-		if Input.is_action_pressed("ui_up") and (movement.jump <= MAX_JUMP):
-			movement.jump += DELTA_JUMP
-		elif Input.is_action_just_released("ui_up") or movement.jump > MAX_JUMP:
-			movement.can_jump = false
-			movement.jump = min(movement.jump, MAX_JUMP)
-	return last_direction
+	if m.can_jump:
+		if Input.is_action_pressed("ui_up") and (m.jump <= MAX_JUMP):
+			m.jump += DELTA_JUMP
+		elif Input.is_action_just_released("ui_up") or m.jump > MAX_JUMP:
+			m.can_jump = false
+			m.jump = min(m.jump, MAX_JUMP)
 
-func calc_movement(delta):
+func calc_movement(delta,m:Movement):
 
 	if input_direction:
-		movement.speed += ACCELERATION * delta
+		m.speed += ACCELERATION * delta
 	else:
-		movement.speed -= FRICTION * delta
+		m.speed -= FRICTION * delta
 		
-	movement.speed = clamp(movement.speed,0,MAX_SPEED)
-	movement.motion.x = movement.speed * movement.direction
+	m.speed = clamp(m.speed,0,MAX_SPEED)
+	m.motion.x = m.speed * m.direction
 	
 	# Movement y
-	if not movement.can_jump and movement.jump > 0:
-		movement.motion.y -= clamp(movement.jump,0,MAX_JUMP)
-		movement.jump = 0
+	if not m.can_jump and m.jump > 0:
+		m.motion.y -= clamp(m.jump,0,MAX_JUMP)
+		m.jump = 0.0
 		
 	# Only add gravity if player is not on the ground
 	# Unfortunatly is_on_floor() is not giving always true if the player moves
@@ -96,26 +89,26 @@ func calc_movement(delta):
 	# Therefore I also test if input movement was applied to force is_on_floor()
 	# will fire next frame
 	if not is_on_floor() or input_direction != 0:
-		movement.motion.y += GRAVITY * delta
+		m.motion.y += GRAVITY * delta
 
 	# Update movement	
-	movement.motion = move_and_slide(movement.motion,UP)
+	m.motion = move_and_slide(m.motion,UP)
 
 
-func play_animation(direction,speed,on_ground):
-	if speed != 0:
-		movement.animationsprite.speed_scale = 1
-		if direction > 0:
-			movement.animationsprite.animation = "RightWalk"
-		elif direction < 0:
-			movement.animationsprite.animation = "LeftWalk"
+func play_animation(m:Movement,on_ground:bool):
+	if m.speed != 0:
+		m.animationsprite.speed_scale = 1
+		if m.direction > 0:
+			m.animationsprite.animation = "RightWalk"
+		elif m.direction < 0:
+			m.animationsprite.animation = "LeftWalk"
 	else:
-		movement.motion.x = 0
-		movement.animationsprite.speed_scale = .25
-		movement.animationsprite.animation = "Idle"
+		m.motion.x = 0
+		m.animationsprite.speed_scale = .25
+		m.animationsprite.animation = "Idle"
 		
 	if  on_ground:
-		movement.animationsprite.playing = true
+		m.animationsprite.playing = true
 	else:
-		movement.animationsprite.frame = 0
-		movement.animationsprite.playing = false
+		m.animationsprite.frame = 0
+		m.animationsprite.playing = false
